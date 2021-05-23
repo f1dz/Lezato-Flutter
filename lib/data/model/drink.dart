52c2bfb80abd189d9ea670dddfe5072de796e554
Khofidin <offiedz@gmail.com>
2021-05-23 11:53:53 +0700

    Add model and api

---
+++ lib/data/model/drink.dart
@@ -0,0 +1,15 @@
+class Drink {
+  Drink({
+    this.name,
+  });
+
+  String name;
+
+  factory Drink.fromJson(Map<String, dynamic> json) => Drink(
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+      };
+}
---
+++ lib/data/model/restaurant.dart
@@ -0,0 +1,41 @@
+import 'menu.dart';
+
+class Restaurant {
+  Restaurant({
+    this.id,
+    this.name,
+    this.description,
+    this.pictureId,
+    this.city,
+    this.rating,
+    this.menus,
+  });
+
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  double rating;
+  Menu menus;
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        pictureId: json["pictureId"],
+        city: json["city"],
+        rating: json["rating"].toDouble(),
+        menus: Menu.fromJson(json["menus"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "pictureId": pictureId,
+        "city": city,
+        "rating": rating,
+        "menus": menus.toJson(),
+      };
+}
