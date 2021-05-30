4d94d1f901ff8fcff91ec287445759182d008004
Khofidin <offiedz@gmail.com>
2021-05-30 18:38:36 +0700

    Remove provider from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lezato/provider/app_provider.dart';
 import 'package:lezato/ui/splash_screen.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppProvider(),
-      child: MaterialApp(
-        title: 'Lezato',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-        home: SplashScreen(),
+    return MaterialApp(
+      title: 'Lezato',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
+      home: SplashScreen(),
     );
   }
 }
